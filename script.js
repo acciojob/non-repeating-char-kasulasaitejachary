e2842bdccb012a48d35904708c21709e3d5d74af
kasulasaitejachary <98974519+kasulasaitejachary@users.noreply.github.com>
2023-09-10 14:47:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let i=0;
+	for(; i<str.length;){
+		if(str.charAt(i)==str.charAtcode(i+1)){
+			i++;
+		}
+		else{
+			console.log(str.charAt(i));
+		}
+	}
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
